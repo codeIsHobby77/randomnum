238be3c70310a7b48564f3f800307c3651641c17
codeIsHobby77 <ayushkhanal265@gmail.com>
2026-01-15 16:01:25 +0545

    Refactor guessing game logic and UI elements

--- chaiaurcode.js
+++ chaiaurcode.js
@@ -1,89 +1,72 @@
-let randomNumber=parseInt(Math.random()*100+1)
+let randomNumber=Math.floor(Math.random()*100)+1
 console.log(randomNumber)
+let para=document.getElementById('p')
+let lowOrHi=document.getElementById('lowOrHi')
+let guessRemain=document.getElementById('guessremain')
+let prevguess=document.getElementById('prevGuess')
 
-const submit = document.querySelector('#subt');
-const guessField=document.querySelector('.guessField')
-const previousGuesses=document.querySelector('.guesses')
-const remainGuess=document.querySelector('.lastResult')
-const lowOrHi=document.querySelector(".lowOrHi")
-const startOver = document.querySelector('.resultParas');
-const p=document.createElement('p')
+array=[]
 
-let previousGuess=[];
-let numGuess=0;
-let playGame=true
 
-if(playGame){
-  submit.addEventListener('click',function(e){
-    e.preventDefault();
-    const guess=parseInt(guessField.value)
-    isGuessValid(guess)
-  })
+
+input=document.getElementById('subt')
+let guessNumber=10
+let guess
+value=input.addEventListener('click',function(e){
+    e.preventDefault()
+    guessField=document.getElementById('guessField').value
+    guess=parseInt(guessField)
+    array.push(guess)
+
+    newNum=array.filter(value=>{
+        return value
+
+    })
+    
+   lastArray= prevguess.innerHTML=`${newNum}`
+   lastArray++
+ 
+    newGuess=guessNumber--
+    guessRemain.innerHTML=`${newGuess}`
+    check()
+})
+
+let isPlaying=true;
+
+function check(){
+    if (guess<0 || guess>100 || isNaN(guess)){
+        lowOrHi.innerHTML=`Invalid credential because guess i.e  is invalid`
+        restart()
+    }
+    else{
+        result()
+    }
 }
-function isGuessValid(guess){
-  if(guess<0){
-    alert("Guess is less than 0")
-  }else if(guess>100){
-    alert("Guess is more than 100")
-  }else if(isNaN(guess)){
-    alert("it is not a number")
-  }
-  else{
-    previousGuess.push(guess)
-    console.log(previousGuess)
-    checkResult(guess)
-  }
-}
-function checkResult(guess){
-  if(guess>randomNumber){
-    displayMessage("Too High",guess)
-  }
-  else if(guess<randomNumber){
-    displayMessage("Too Low",guess) 
-  }
-  else if(guess===randomNumber){
-    displayMessage("Congrats Bro Superb🔥👏 You guessed",guess)
-  }
-}
-
-function displayMessage(textMessage,guess){
-  guessField.value = '';
-  previousGuesses.textContent = previousGuess.join(" ")
-
-  showResultHighOrLow(textMessage,guess)
-}
-
-function showResultHighOrLow(textMessage,guess){
-  lowOrHi.innerHTML=`${textMessage}`
-  numGuess++
-  remainNum=remainGuess.innerHTML=`${10-numGuess}`
-  realNum=parseInt(remainNum)
-  endgame(realNum,guess)
-}
-function endgame(realNum, guess){
-  if(realNum === 0){
-    startGame();  // reset only if attempts run out
-  }
-  else if(guess === randomNumber){
-    // Show congrats message and stop further guessing
-    lowOrHi.innerHTML = "Congrats Bro Superb🔥👏 You guessed";
-    previousGuess=[]
-    numGuess = 0;
-    previousGuesses.textContent = '';
-    remainGuess.textContent = '10';
-
-  }
+function result(){
+    if(newGuess===0){
+          lowOrHi.innerHTML=`Sorry You lose random Number was ${randomNumber}`
+    }
+   else if (guess<randomNumber) {
+            lowOrHi.innerHTML=`Tooo Low Try Again` 
+    }
+    else if(guess>randomNumber){
+                 lowOrHi.innerHTML=`Tooo High Try Again` 
+    }
+    else if(guess==randomNumber){
+          lowOrHi.innerHTML="Correct Congrats Superb🩵🩵"
+          restart()
+            guessNumber=10
+    }
+ if( newGuess==0){
+    restart()
 }
 
-
-  function startGame(){
-    previousGuess = [];
-    numGuess = 0;
-    previousGuesses.textContent = '';
-    lowOrHi.textContent = '';
-    remainGuess.textContent = '10';
-    randomNumber = parseInt(Math.random() * 100 + 1);
-    guessField.disabled = false;
-    submit.disabled = false;
-  }
   
+}
+function restart(){
+    guessNumber=10
+    document.getElementById('guessField').value=""
+    randomNumber=Math.floor(Math.random()*100)+1
+    lastArray= prevguess.innerHTML=""
+}
+
